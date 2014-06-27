227c89af1bb56863c6241195aa7bacae946a01d0
Adam Walsh <adamwalsh14@gmail.com>
2014-06-27 19:52:14 -0400

    Inital commit of run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#Written by: Adam Walsh
+#Written on 6/27/14
+echo "py2app-OSX helper running..."
+echo "Opening base launching script for editing. Please change 'FILE_NAME_HERE.py' to your main python files name"
+open py2app-OSX.scpt
+echo "Please export the script and save as type Application"
+echo "Then just zip with your python files and distribute your app"
+
